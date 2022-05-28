00a1ca94687d2793538b23e04c9e04d02ff8a5b7
epshita-shima <epshitashima@gmail.com>
2022-05-28 20:09:16 +0600

    added api to get single product

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ async function run() {
             const products = await cursor.toArray();
             res.send(products);
         });
+
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productsCollection.findOne(query);
+            res.send(product);
+        })
     }
     finally {
 
